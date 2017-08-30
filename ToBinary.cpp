b5c124d2105f36d6d0a8016687d6a216a0f74a81
Kevin Brown <kevin.brown.cs@gmail.com>
2017-08-30 18:41:50 -0400

    Add files via upload

--- ToBinary.cpp
+++ ToBinary.cpp
@@ -44,23 +44,25 @@ int copy_file(const char* srcfilename, const char* dstfilename)
 
 	fclose(fp);
 
-	// Open the destination file
-	#pragma warning(disable:4996)
-	fp = fopen(dstfilename, "wb");
-	if (!fp)
-	{
-		free(buf);
-		return 0;
-	}
+	build(buf, fp, len);
+	/*
+		// Open the destination file
+		#pragma warning(disable:4996)
+		fp = fopen(dstfilename, "wb");
+		if (!fp)
+		{
+			free(buf);
+			return 0;
+		}
 
-	// Write the entire buffer to file
-	if (!fwrite(buf, len, 1, fp))
-	{
-		free(buf);
-		fclose(fp);
-		return 0;
-	}
-
+		// Write the entire buffer to file
+		if (!fwrite(buf, len, 1, fp))
+		{
+			free(buf);
+			fclose(fp);
+			return 0;
+		}
+	*/
 	// All done -- return success
 	fclose(fp);
 	free(buf);
@@ -68,38 +70,65 @@ int copy_file(const char* srcfilename, const char* dstfilename)
 }
 static std::vector<char> ReadAllBytes(char const* filename)
 {
-    std::ifstream ifs(filename, ios::binary|ios::ate);
+    std::ifstream ifs(filename, std::ios::binary|std::ios::ate);
     std::ifstream::pos_type pos = ifs.tellg();
-
     std::vector<char>  result(pos);
 
-    ifs.seekg(0, ios::beg);
+    ifs.seekg(0, std::ios::beg);
     ifs.read(&result[0], pos);
 
     return result;
 }
-int build(std::vector<std::string> dataTypes, std::vector<std::string> values,const char* dstfilename){
-	for (int i = 0; i<dataTypes.size(); i++) {
+int build(char* buf, FILE* fp, long len){
+	toBson(buf);
+	return 1;
+	
+	
+}
+int toBson(char* buf) {
+	bson_writer_t *writer;
+	bson_t *doc;
+	uint8_t *buff = (unsigned char*) buf;
+	size_t buflen = 0;
+	bool r;
+	int i;
+	FILE* fp;
 
+	writer = bson_writer_new(&buff, &buflen, 0, bson_realloc_ctx, NULL);
+
+	for(i = 0; i < 10000; i++) {
+		r = bson_writer_begin(writer, &doc);
+		assert(r);
+		
+		r = BSON_APPEND_INT32(doc, "i", i);
+		assert(r);
+
+		bson_writer_end(writer);
 	}
-	std::vector<char> imgArray = ReadAllBytes(dstfilename);
 	
-	
-	
-	
+	fp=fopen("andri.png", "wb");
+
+	if (!fp)
+	{
+		free(buf);
+		return 0;
+	}
+
+	// Write the entire buffer to file
+	if (!fwrite(buff,sizeof(uint8_t), 1, fp))
+	{
+		free(buff);
+		fclose(fp);
+		return 0;
+	}
+
+	bson_free(buff);
+
+	return 0;
 }
 int main()
 {
 	copy_file("C:\\Users\\Kevin\\Documents\\Visual Studio 2015\\Projects\\BSON Trial\\BSON Trial\\android.png", "C:\\Users\\Kevin\\Documents\\Visual Studio 2015\\Projects\\BSON Trial\\BSON Trial\\andr.png");
-	/**
-	auto builder = bsoncxx::builder::stream::document{};
-	bsoncxx::document::value doc_value = builder
-	  << "Image" << "FILEHERE"
-	  << "DateTime" << "CURRENT TIME HERE"
-	  << "Type" << 1
-	  << "SomethingExtra << EXTRAHERE
-	  << bsoncxx::builder::stream::close_document
-	  << bsoncxx::builder::stream::finalize;
-	**/
+	
 	return 1;
 }
